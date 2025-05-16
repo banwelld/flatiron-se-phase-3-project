02a4d37c2cb0313983d2bfb7f93b75945a2bc7a5
Dave Banwell <dave@davebanwell.com>
2025-05-15 23:33:23 -0400

    No more typos in db_init filepath

--- lib/data/setup/db_initializer.py
+++ lib/data/setup/db_initializer.py
@@ -3,7 +3,7 @@
 import sys
 from pathlib import Path
 
-sys.path.append(str(Path(__file__).resolve().parent.paren.parent))
+sys.path.append(str(Path(__file__).resolve().parent.parent.parent))
 
 from models import Participant, Team
 
