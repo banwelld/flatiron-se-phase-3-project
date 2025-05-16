2ce5ce22fe94bf083e1ab01ff443d7b19d901759
Dave Banwell <dave@davebanwell.com>
2025-05-15 23:26:04 -0400

    all files outfitted with os or pathlib filepath routing

--- lib/data/setup/db_initializer.py
+++ lib/data/setup/db_initializer.py
@@ -3,7 +3,7 @@
 import sys
 from pathlib import Path
 
-sys.path.append(str(Path(__file__).resolve().parent.parent))
+sys.path.append(str(Path(__file__).resolve().parent.paren.parent))
 
 from models import Participant, Team
 
--- lib/data/setup/db_seeder.py
+++ lib/data/setup/db_seeder.py
@@ -3,7 +3,7 @@
 import sys
 from pathlib import Path
 
-sys.path.append(str(Path(__file__).resolve().parent.parent))
+sys.path.append(str(Path(__file__).resolve().parent.parent.parent))
 
 from models import Participant, Team
 
